c04f1abadcfad4bb5259ef1f61bc55818d87b1de
Rafy Hany said ansi abdelmalak <121082388+RafyHany@users.noreply.github.com>
2026-05-09 19:20:26 +0300

    setup

---
+++ Centeral_Station/src/main/java/com/example/Centeral_Station/CenteralStationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Centeral_Station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenteralStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenteralStationApplication.class, args);
+	}
+
+}
